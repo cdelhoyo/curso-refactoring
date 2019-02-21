a735b4883bf3f8bb59c54c3c81e4b41d991ce3ca
Carlos del Hoyo Muñoz <cdelhoyo@autentia.com>
2019-02-21 12:04:16 +0100

    Creamos mapa mapaDeCadaProductoConLaCantidadDeVecesRepetido y calculamos precio total con lambda, pero fallan los test de segundo a mitad de precio :(

--- src/main/java/es/cdelhoyo/refactoring/Calculadora.java
+++ src/main/java/es/cdelhoyo/refactoring/Calculadora.java
@@ -6,57 +6,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Calculadora {
 
     public double calcular(List<Producto> productos, Comprador comprador){
-        List<String> nombres = new ArrayList(); // nombres diferentes de los productos
-        for (Producto p: productos){
-            if(!nombres.contains(p.getNombre())) {
-                nombres.add(p.getNombre());
-            }
-        }
-
-        Map<String, List<Producto>> productosMap = new HashMap(); // mapa con la lista de productos que tienen el mismo nombre
-        for(String n:nombres){
-            List<Producto> productosConNombre = new ArrayList();
-            for (Producto p: productos){
-                if(p.getNombre().equals(n)){
-                    productosConNombre.add(p);
-                }
-            }
-            productosMap.put(n, productosConNombre);
-        }
-
-        List<Producto> productosConElSegundoAMitadDePrecioAplicado = new ArrayList();
-        for (List<Producto> productosConNombre: productosMap.values()){
-            for(int i=0; i< productosConNombre.size(); i++){
-                Producto p = productosConNombre.get(i);
-                if(i%2==0){ // Un producto que está en posicines pares nunca va a ser el segundo y nunca tendra rebaja de segundo a mitad de precio
-                    productosConElSegundoAMitadDePrecioAplicado.add(p);
-                }else{
-                    if(productosConNombre.get(i).getSegundoAMitadDePrecio()){
-                        p.setPrecio(p.getPrecio()/2);
-                    }
-                    productosConElSegundoAMitadDePrecioAplicado.add(p);
-                }
-            }
-        }
-
-        // En este momento ya tenemos la lista de productos igual que la que entra,
-        // pero ya se ha aplicado la rebaja de mitad de precio a los productos que incluian la oferta
-        // Esta lista la hemos dejado en productosConElSegundoAMitadDePrecioAplicado
-
-        double precioTotal = 0.0;
-        for (Producto producto: productosConElSegundoAMitadDePrecioAplicado){
-            double precioTotalDelProducto = calcularPrecioTotalDelProducto(comprador, producto);
-            precioTotal+=precioTotalDelProducto;
-        }
+        Map<Producto, Long> mapaDeCadaProductoConLaCantidadDeVecesRepetido = productos.stream().collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+        double precioTotal = mapaDeCadaProductoConLaCantidadDeVecesRepetido.keySet().stream()
+                .mapToDouble(producto -> calcularPrecioFinalDeTodasLasUnidadesDelProducto(comprador, producto, mapaDeCadaProductoConLaCantidadDeVecesRepetido.get(producto)))
+                .sum();
         return redondearADosDecimales(precioTotal);
     }
 
-    private double calcularPrecioTotalDelProducto(Comprador comprador, Producto producto) {
-        double precioDelProductoConRebaja = producto.getPrecio() * producto.getRebaja();
+    private double calcularPrecioFinalDeTodasLasUnidadesDelProducto(Comprador comprador, Producto producto, Long cantidad) {
+        double precioDelProductoConRebaja = producto.getPrecio() * producto.getRebaja() * cantidad;
         double decuentoDeEdad = calcularDecuentoDeEdad(comprador, producto);
         double precioDelProductoConRebajaYDescuentoDeEdad = precioDelProductoConRebaja * decuentoDeEdad;
         double porcentajeDeIVAPorIVA = calcularPorcentajeDeIVAPorIVA(producto);
